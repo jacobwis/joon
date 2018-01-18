23a05856a52b87c5420fd54e819b9b8ce67f7084
Jacob Wisniewski <wisniewskij26@gmail.com>
2018-01-18 00:08:14 -0600

    add --env option to all CLI commands

--- bin/joon.js
+++ bin/joon.js
@@ -4,19 +4,23 @@ const Joon = require('../dist');
 
 let joon;
 
-const executeCommand = async command => {
-  joon = await Joon.createInstance();
+const executeCommand = async (command, env = 'development') => {
+  joon = await Joon.createInstance(env);
   await command();
   await joon.pool.end();
 };
 
 (() => {
   try {
-    app.command('up').action(async () => {
-      const joon = await Joon.createInstance();
-      await joon.up();
-      await joon.pool.end();
-    });
+    app
+      .command('up')
+      .option('-e, --env [environment]', '', val => val, 'development')
+      .action(async ({ env }) => {
+        console.log(env);
+        await executeCommand(async () => {
+          await joon.up();
+        }, env);
+      });
 
     app.command('create [name]').action(async name => {
       await executeCommand(async () => {
@@ -28,17 +32,22 @@ const executeCommand = async command => {
     app
       .command('down')
       .option('-c, --count [count]')
-      .action(async ({ count }) => {
+      .option('-e, --env [environment]', '', val => val, 'development')
+      .action(async ({ count, env }) => {
+        console.log(env);
         await executeCommand(async () => {
           await joon.down(count || 1);
-        });
+        }, env);
       });
 
-    app.command('reset').action(async () => {
-      await executeCommand(async () => {
-        await joon.reset();
+    app
+      .command('reset')
+      .option('-e, --env [environment]', '', val => val, 'development')
+      .action(async ({ env }) => {
+        await executeCommand(async () => {
+          await joon.reset();
+        }, env);
       });
-    });
 
     app.parse(process.argv);
   } catch (e) {
