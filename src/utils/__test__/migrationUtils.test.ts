bbb07fe7e5c522644d6087e2308b3b5c225b6ede
Jacob Wisniewski <wisniewskij26@gmail.com>
2018-01-17 20:57:45 -0600

    add migration utilities

--- src/utils/__test__/migrationUtils.test.ts
+++ src/utils/__test__/migrationUtils.test.ts
@@ -1,6 +1,12 @@
+import * as mock from 'mock-fs';
 import * as testUtils from '../../../test/utils';
 import * as db from '../../db';
-import { getCompletedMigrations } from '../migrationUtils';
+import {
+  getCompletedMigrations,
+  getPendingMigrations,
+  parseMigration,
+  formatSQL
+} from '../migrationUtils';
 
 beforeAll(() => {
   db.initPool({
@@ -20,5 +26,82 @@ describe('getCompletedMigrations', () => {
   it('should return an array of all migrations from the database', async () => {
     const migrations = await getCompletedMigrations();
     expect(migrations).toEqual(['CreateUserTable.sql', 'CreatePostTable.sql']);
+  });
+});
+
+describe('getPendingMigrations', () => {
+  let pendingMigrations;
+
+  beforeAll(async () => {
+    await testUtils.setupTestDB();
+
+    mock({
+      migrations: {
+        'CreateUserTable.sql': '',
+        'CreatePostTable.sql': '',
+        'CreateBookTable.sql': '',
+        'CreateUpvoteTable.sql': '',
+        'randomFile.js': ''
+      }
+    });
+
+    pendingMigrations = await getPendingMigrations();
+  });
+
+  afterAll(() => {
+    mock.restore();
+  });
+
+  it('should include migrations from the migrations directory that are not in the migrations table', () => {
+    expect(pendingMigrations).toContainEqual('CreateBookTable.sql');
+    expect(pendingMigrations).toContainEqual('CreateUpvoteTable.sql');
+  });
+
+  it('should not include migrations that are in the migrations table', () => {
+    expect(pendingMigrations).not.toContainEqual('CreateUserTable.sql');
+    expect(pendingMigrations).not.toContainEqual('CreatePostTable.sql');
+  });
+
+  it('should not include any files that are not .sql files', () => {
+    expect(pendingMigrations).not.toContainEqual('randomFile.js');
+  });
+});
+
+describe('parseMigration', () => {
+  const testMigration = `
+  /* UP */
+  create table posts(
+    id serial primary key,
+    content varchar
+  );
+  /* DOWN */
+  drop table posts;
+  `;
+
+  it("should return the correct 'up' value", () => {
+    const { up } = parseMigration(testMigration);
+    expect(up).toEqual(
+      'create table posts(id serial primary key, content varchar);'
+    );
+  });
+
+  it("should return the correct 'down' value", () => {
+    const { down } = parseMigration(testMigration);
+    expect(down).toEqual('drop table posts;');
+  });
+});
+
+describe('formatSQL', () => {
+  it('should correctly format a raw SQL string', () => {
+    const sql = `
+    create table posts(
+      id serial primary key,
+      content varchar
+    );    
+    `;
+
+    expect(formatSQL(sql)).toEqual(
+      'create table posts(id serial primary key, content varchar);'
+    );
   });
 });
--- src/utils/migrationUtils.ts
+++ src/utils/migrationUtils.ts
@@ -1,6 +1,55 @@
+import * as path from 'path';
+import * as fs from 'fs';
+import { promisify } from 'util';
 import * as db from '../db';
+
+const readdir = promisify(fs.readdir);
 
 export const getCompletedMigrations = async () => {
   const { rows } = await db.query('SELECT * FROM migrations');
   return rows.map(row => row.name);
+};
+
+export const getPendingMigrations = async () => {
+  const migrationsDir = path.resolve(process.cwd(), 'migrations');
+
+  const completedMigrations = await getCompletedMigrations();
+  const migrations = await readdir(migrationsDir);
+
+  return migrations.filter(migration => {
+    if (path.extname(migration) !== '.sql') {
+      return false;
+    }
+
+    return completedMigrations.indexOf(migration) === -1;
+  });
+};
+
+export const parseMigration = (contents: string) => {
+  const START_TOKEN = '/* UP */';
+  const DOWN_TOKEN = '/* DOWN */';
+
+  const up = contents.substring(
+    contents.indexOf(START_TOKEN) + START_TOKEN.length,
+    contents.indexOf(DOWN_TOKEN)
+  );
+
+  const down = contents.substring(
+    contents.indexOf(DOWN_TOKEN) + DOWN_TOKEN.length
+  );
+
+  return {
+    up: formatSQL(up),
+    down: formatSQL(down)
+  };
+};
+
+export const formatSQL = (contents: string) => {
+  return contents
+    .trim()
+    .split('\n')
+    .map(line => line.trim())
+    .join(' ')
+    .replace('( ', '(')
+    .replace(' )', ')');
 };
